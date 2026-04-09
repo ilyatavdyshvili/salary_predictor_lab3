97468dc13787225675d8e98490ac4b3063ced1bd
ilyatavdyshvili <wo.ilya.tavdyshvili.rk@gmail.com>
2026-04-09 14:42:26 +0400

    Update services.py

--- src/application/services.py
+++ src/application/services.py
@@ -3,9 +3,12 @@ import subprocess
 
 class DataSyncService:
     def sync_dataset(self):
-        subprocess.run(
-            ["dvc", "pull"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            check=True
-        )
+        try:
+            subprocess.run(
+                ["dvc", "pull"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                check=True
+            )
+        except subprocess.CalledProcessError:
+            pass
