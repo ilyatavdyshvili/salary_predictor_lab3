a8eb2bb8d63fbe5ebbe3699062c8e6fb1464ba2b
ilyatavdyshvili <wo.ilya.tavdyshvili.rk@gmail.com>
2026-04-09 14:34:13 +0400

    Update services.py

--- src/application/services.py
+++ src/application/services.py
@@ -3,13 +3,9 @@ import subprocess
 
 class DataSyncService:
     def sync_dataset(self):
-        print("[Sync] Running dvc pull...")  # можно оставить или убрать
-
         subprocess.run(
             ["dvc", "pull"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
             check=True
         )
-
-        print("[Sync] Dataset is up to date")
